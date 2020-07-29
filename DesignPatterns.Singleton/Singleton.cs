caafe2353cccba42e8f656e6dd54375c04131e49
Mateus Bispo <math.l.bispo@gmail.com>
2020-07-28 21:46:21 -0300

    implementando padrão singleton

---
+++ DesignPatterns.Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton Instance = null;
+
+        public static Singleton GetInstance 
+        { 
+            get
+            {
+                if (Instance == null)
+                    Instance = new Singleton();
+                return Instance;
+            }               
+        }
+    }
+}
